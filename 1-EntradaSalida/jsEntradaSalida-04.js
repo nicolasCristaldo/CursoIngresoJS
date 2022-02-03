c11f23a502ecf5809bf833dc9870fb48e259542b
nicolas cristaldo <crist4545@gmail.com>
2022-02-03 18:06:01 -0300

    e/s de datos ej 4

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -1,9 +1,12 @@
-/* 
+/* Nicolás Cristaldo - ejercicio 4
 Debemos lograr tomar un dato por 'PROMPT' 
 y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+	let dato;
+	dato = prompt("ingrese un dato");
+
+    document.getElementById("txtIdNombre").value = dato;
 }
 
