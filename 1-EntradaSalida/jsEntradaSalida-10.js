30e0522e991d066f0d6ca2e59d2f690041ac905d
nicolas cristaldo <crist4545@gmail.com>
2022-02-05 14:26:18 -0300

    e/s de datos ej 10 bis 2

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -1,18 +1,30 @@
-/* Nicolás Cristaldo - ejercicio 10
-Debemos lograr tomar el importe por ID.
-Transformarlo a entero (parseInt), luego
-mostrar el importe con un Descuento del 25 %
-en el cuadro de texto "RESULTADO"*/
+/* Nicolás Cristaldo - ejercicio 10 bis 2
+se debe pedir el nombre del producto al usuario, 
+el importe y tambien se debe pedir el porcentaje de descuento al usuario, 
+se debe: mostrar el importe final por ID y ademas mostrar el mensaje:  
+"Usted compro un XXXXXX con XX % de descuento, el precio final es XXXX" */
 function mostrarAumento()
 {
-	let importe;
+	let nombreProducto;
+	let importe; 
+	let sacarPorcentaje;
+	let porcentaje;
 	let descuento;
 	let resultado;
+	let mostrarMensaje;
 
 	importe = parseInt(document.getElementById("txtIdImporte").value);
 
-	descuento = importe * 0.25;
+	nombreProducto = prompt("ingrese el nombre del producto");
+
+	sacarPorcentaje = prompt("¿cuál es el % de descuento? En caso que el número sea menor a 10 agregarle un 0 por delante");
+	porcentaje = parseFloat("0." + sacarPorcentaje);
+    
+    descuento = importe * porcentaje;
 	resultado = importe - descuento;
+	mostrarMensaje = "usted compró un " + nombreProducto + " con " + sacarPorcentaje + "% de descuento, el precio final es: " + resultado;
 
 	document.getElementById("txtIdResultado").value = resultado;
+
+	alert(mostrarMensaje);
 }
