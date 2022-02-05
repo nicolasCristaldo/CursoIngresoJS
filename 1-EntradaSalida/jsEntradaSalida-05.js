4256ff1cb92ace2d0045ec78665da273b99f3218
nicolas cristaldo <crist4545@gmail.com>
2022-02-05 12:26:18 -0300

    e/s de datos ej 5-bis

--- 1-EntradaSalida/jsEntradaSalida-05.js
+++ 1-EntradaSalida/jsEntradaSalida-05.js
@@ -1,15 +1,18 @@
-/* Nicolás 1cristaldo - ejercicio 5
-Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
-ej.: "Usted se llama José y tiene 66 años" 	*/
+/* Nicolás 1cristaldo - ejercicio 5 bis
+Se debe mostrar el mensaje:
+ "Perez, usted se llama Jose y tiene 66 años" se debe obtener el apellido , de la manera que puedan... 	*/
 function mostrar()
 {	
 	let nombre;
 	let edad;
+	let apellido;
 	let mensaje;
+
+    apellido = prompt("ingrese su apellido");
 
 	nombre = document.getElementById("txtIdNombre").value;
 	edad = document.getElementById("txtIdEdad").value;
-	mensaje = "usted se llama " + nombre + " y tiene " + edad + " años.";
+	mensaje = apellido + ", " + "usted se llama " + nombre + " y tiene " + edad + " años.";
 	
 	alert(mensaje);
 }
