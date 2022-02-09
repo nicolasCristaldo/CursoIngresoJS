7067c01832bb8b4c21dab994ff52de3b67d87717
nicolas cristaldo <crist4545@gmail.com>
2022-02-09 16:14:05 -0300

    if ej 1

--- 2-InstruccionIf/jsInstruccionIF-01.js
+++ 2-InstruccionIf/jsInstruccionIF-01.js
@@ -1,6 +1,16 @@
+// Nicolás Cristaldo - if ejercicio 1
 function mostrar()
 {
+	//declaro variable
+	var edadIngresada;
+
 	//tomo la edad  
-	alert("ok");
+
+    edadIngresada = parseInt(document.getElementById("txtIdEdad").value);
+
+    if(edadIngresada == 15){
+       alert("niña bonita");
+    }
+	
 
 }//FIN DE LA FUNCIÓN
