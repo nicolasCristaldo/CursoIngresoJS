4056da91389dda0e76d9f81ff46541ebec15a145
nicolas cristaldo <crist4545@gmail.com>
2022-02-10 16:30:32 -0300

    if ejercicio 8

--- 2-InstruccionIf/jsInstruccionIF-08.js
+++ 2-InstruccionIf/jsInstruccionIF-08.js
@@ -1,8 +1,16 @@
+// Nicolás Cristaldo - if ejercicio 8
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
-	
+	let edad;
+	let estadoCivil;
+
+	edad = parseInt(document.getElementById("txtIdEdad").value);
+	estadoCivil = document.getElementById("estadoCivil").value;
+
+
+	if(edad >= 18 && estadoCivil == "Soltero"){
+		alert("Es soltero y no es menor");
+	}
 
 
 }//FIN DE LA FUNCIÓN
