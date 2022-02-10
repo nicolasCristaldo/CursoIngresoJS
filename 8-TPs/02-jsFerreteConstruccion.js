78986e25a48dc600e8767bbc1f38f063762a495b
nicolas cristaldo <crist4545@gmail.com>
2022-02-09 22:03:08 -0300

    tp 2

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -1,18 +1,67 @@
-/*2.	Para el departamento de Construcción:
-A.	 mostrar la cantidad de alambre a comprar  si se ingresara el largo y el ancho de un terreno rectangular y se debe alambra con tres hilos de alambre.
-B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
-C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
+/* Nicolás cristaldo - tp 2
+2.	Para el departamento de Construcción:
+A.	 mostrar la cantidad de alambre a comprar  si se ingresara el largo y el ancho de un terreno
+ rectangular y se debe alambra con tres hilos de alambre.
+B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y
+ se debe alambra con tres hilos de alambre.
+C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar
+ cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
 */
 
 function Rectangulo () 
 {
+	let largo;
+	let ancho;
+	let perimetro;
+	let cantidadAlambre;
+	let mostrarMensaje;
+
+	largo = parseFloat(document.getElementById("txtIdLargo").value);
+	ancho = parseFloat(document.getElementById("txtIdAncho").value);
+
+	perimetro = (largo * 2) + (ancho * 2);
+	cantidadAlambre = perimetro / 3;
+
+	mostrarMensaje = "se tienen que comprar 3 hilos de alambre de " + cantidadAlambre.toFixed(2) + " metros cada uno."
+
+	alert(mostrarMensaje);
 
 }
 function Circulo () 
 {
-	
+	let radio;
+	let perimetro;
+	let cantidadAlambre;
+	let mostrarMensaje;
+
+	radio = parseFloat(document.getElementById("txtIdRadio").value);
+
+	perimetro = 2 * 3.14 * radio;
+	cantidadAlambre = perimetro / 3;
+
+	mostrarMensaje = "se tienen que comprar 3 hilos de alambre de " + cantidadAlambre.toFixed(2) + " metros cada uno."
+
+	alert(mostrarMensaje);
+
 }
 function Materiales () 
 {
-	
+	let largo;
+	let ancho;
+	let area;
+	let bolsasDeCal;
+	let bolsasDeCemento; 
+	let mostrarMensaje;
+
+	largo = parseFloat(document.getElementById("txtIdLargo").value);
+	ancho = parseFloat(document.getElementById("txtIdAncho").value);
+
+	area = largo * ancho;
+	bolsasDeCemento = area * 2;
+	bolsasDeCal = area * 3;
+
+	mostrarMensaje = "se necesitan " + bolsasDeCemento + " bolsas de cemento y " + bolsasDeCal + " bolsas de cal";
+
+	alert(mostrarMensaje);
+
 }
