f64b26528ed9b315e57f242c2c90a4d7cdb97264
nicolas cristaldo <crist4545@gmail.com>
2022-02-11 16:47:06 -0300

    switch ejercicio 4

--- 3-InstruccionSwitch/jsInstruccionSwitch-04.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-04.js
@@ -1,11 +1,23 @@
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
-	
-	
+	let mes;
 
+	mes = document.getElementById("txtIdMes").value;
+
+	switch(mes){
+		case "Abril":
+		case "Junio":
+		case "Septiembre":
+		case "Noviembre":
+			alert("tiene 30 días");
+			break;
+		case "Febrero":
+			alert("tiene 28 días");
+			break;
+		default:
+			alert("tiene 31 días");
+			break;
+	}
 
 
 }//FIN DE LA FUNCIÓN
