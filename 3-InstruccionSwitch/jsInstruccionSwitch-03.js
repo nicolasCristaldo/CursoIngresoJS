b89a0b88403b823b385b1ac6d9a1fda8ceb77c4a
nicolas cristaldo <crist4545@gmail.com>
2022-02-11 16:31:24 -0300

    switch ejercicio 3

--- 3-InstruccionSwitch/jsInstruccionSwitch-03.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-03.js
@@ -1,9 +1,17 @@
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
-	
+	let mes;
+
+	mes = document.getElementById("txtIdMes").value;
+
+	switch(mes){
+		case "Febrero":
+			alert("Este mes no tiene más de 29 días.");
+			break;
+	default:
+			alert("Este mes tiene 30 o más días");
+			break;
+	}
 	
 
 
