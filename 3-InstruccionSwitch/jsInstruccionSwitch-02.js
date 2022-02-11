4e52316920ea4965932d37fe078a4b4ace0feca1
nicolas cristaldo <crist4545@gmail.com>
2022-02-11 16:04:31 -0300

    switch ejercicio 2

--- 3-InstruccionSwitch/jsInstruccionSwitch-02.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-02.js
@@ -1,8 +1,33 @@
+//Nicolás Cristaldo - switch ejercicio 2
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	let mes;
+
+	mes = document.getElementById("txtIdMes").value;
+
+	switch(mes){
+		case "Enero":
+			
+		case "Febrero":
+			
+	    case "Marzo":
+			
+		case "Abril":
+			
+		case "Mayo":
+			
+		case "Junio":
+			alert("falta para el invierno");
+			break;
+		case "Julio":
+			
+		case "Agosto":
+			alert( "Abrigate que hace frio.");
+			break;
+		default:
+			alert("ya pasamos el frio, ahora calor");
+			break;
+	}
 
 
 
