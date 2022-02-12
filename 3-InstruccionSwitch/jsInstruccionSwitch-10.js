7cc8fd23091d3cb82cbf875b0a23e19074ac1452
nicolas cristaldo <crist4545@gmail.com>
2022-02-12 12:54:52 -0300

    switch ejercicio 10

--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,6 +1,51 @@
+//Nicolás Cristaldo - switch ejercicio 10
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	let estacion;
+	let destino;
+	let mensajeNo;
+	let mensajeSi;
 
+	destino = document.getElementById("txtIdDestino").value;
+	estacion = document.getElementById("txtIdEstacion").value;
+
+	mensajeNo = "no se viaja";
+	mensajeSi = "se viaja";
+
+	switch(estacion){
+		case "Invierno":
+			switch(destino){
+				case "Bariloche":
+					alert(mensajeSi);
+					break;
+				default:
+					alert(mensajeNo);
+					break;
+			}
+			break;
+		 case "Primavera":
+		 	switch(destino){
+		 		case "Bariloche":
+		 			alert(mensajeNo);
+		 			break;
+		 		default:
+		 			alert(mensajeSi);
+		 			break;
+		 	}
+		 	break;
+		 case "Verano":
+		 	switch(destino){
+		 		case "Mar del plata":
+	 			case "Cataratas":
+		 			alert(mensajeSi);
+		 			break;
+		 		default:
+		 			alert(mensajeNo);
+		 			break;
+		 	}
+		 	break;
+		 default:
+		 	alert(mensajeSi);
+		 	break;
+	}
 }//FIN DE LA FUNCIÓN
