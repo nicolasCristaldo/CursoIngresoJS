d62ea0d3e60a341c9f9071a3fd0c0fe42bdd24e1
nicolas cristaldo <crist4545@gmail.com>
2022-02-12 12:17:52 -0300

    switch ejercicio 8

--- 3-InstruccionSwitch/jsInstruccionSwitch-08.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-08.js
@@ -1,6 +1,18 @@
+//Nicolás Cristaldo - switch ejercicio 8
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	let destino;
+
+	destino = document.getElementById("txtIdDestino").value;
+
+	switch(destino){
+		case "Bariloche":
+		case "Ushuaia":
+			alert("hace frio");
+			break;
+		default:
+			alert("hace calor");
+			break;
+	}
 
 }//FIN DE LA FUNCIÓN
