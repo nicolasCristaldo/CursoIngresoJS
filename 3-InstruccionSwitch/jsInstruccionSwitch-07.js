0559af0b3e37825851a7c2a1a8f865db34811871
nicolas cristaldo <crist4545@gmail.com>
2022-02-12 12:13:55 -0300

    switch ejercicio 7

--- 3-InstruccionSwitch/jsInstruccionSwitch-07.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-07.js
@@ -1,6 +1,23 @@
+//Nicolás Cristaldo - switch ejercicio 7
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	let destino;
+
+	destino = document.getElementById("txtIdDestino").value;
+
+	switch(destino){
+		case "Bariloche":
+			alert("se encuentra al oeste");
+			break;
+		case "Ushuaia":
+			alert("se encuentra al sur");
+			break;
+		case "Cataratas":
+			alert("se encuentra al norte");
+			break;
+		default:
+			alert("se encuentra al este");
+			break;
+	}
 
 }//FIN DE LA FUNCIÓN
