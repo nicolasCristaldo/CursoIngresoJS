d06bb6930d087a78770dea9fd1c5798542b369a9
nicolas cristaldo <crist4545@gmail.com>
2022-02-17 15:42:56 -0300

    while 2

--- 5-InstruccionWhile/jsIteracionesWhile-02.js
+++ 5-InstruccionWhile/jsIteracionesWhile-02.js
@@ -1,10 +1,14 @@
+//nicolas cristaldo - while 2
 /*
 al presionar el botón mostrar 10 repeticiones 
 con números DESCENDENTES, desde el 10 al 1.*/
 function mostrar()
 {
-	var contador;
-	contador=0; 
-	alert('iteración while');
+	let i = 11;
+
+	while(i > 1){
+		i = i - 1;
+		alert(i);
+	}
 
 }//FIN DE LA FUNCIÓN
