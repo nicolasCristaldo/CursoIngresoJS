d2cb749f66e458a0bd4273b4fbde4b3f979871a2
nicolas cristaldo <crist4545@gmail.com>
2022-02-17 14:42:31 -0300

    ejercicio 6 con if

--- 3-InstruccionSwitch/jsInstruccionSwitch-06.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-06.js
@@ -1,44 +1,20 @@
-//Nicolás Cristaldo - switch ejercicio 6
+//Nicolás Cristaldo - switch ejercicio 6 con if
 function mostrar()
 {
 	let hora;
 
-	hora = parseInt(document.getElementById("txtIdHora").value);
+	hora = parseFloat(document.getElementById("txtIdHora").value);
 
-	switch(hora){
-		case 7:
-		case 8:
-		case 9:
-		case 10:
-		case 11:
-			alert("es de mañana");
-			break;
-		case 12:
-		case 13:
-		case 14:
-		case 15:
-		case 16:
-		case 17:
-		case 18:
-		case 19:
-			alert("es de tarde");
-				break;
-		case 20:
-		case 21:
-		case 22:
-		case 23:
-		case 24:
-		case 0:
-		case 1:
-		case 2:
-		case 3:
-		case 4:
-		case 5:
-		case 6:
-			alert("es de noche");
-			break;
-		default:
-			alert("la hora no exisite");
-			break;
+	if(hora >= 7 && hora <= 11){
+		alert("es de mañana");
+	}
+	else if(hora >= 12 && hora <=19){
+		alert("es de tarde");
+	}
+	else if(hora >= 0 && hora <= 24){
+		alert("es de noche");
+	}
+	else{
+		alert("esa hora no existe");
 	}
 }//FIN DE LA FUNCIÓN
