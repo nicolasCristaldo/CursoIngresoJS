a8179cddcb77318831f1a6939c736440ecef502e
nicolas cristaldo <crist4545@gmail.com>
2022-02-18 11:52:48 -0300

    while 8

--- 5-InstruccionWhile/jsIteracionesWhile-08.js
+++ 5-InstruccionWhile/jsIteracionesWhile-08.js
@@ -1,19 +1,24 @@
-/*
+/*nicolas cristaldo - while 8
 Al presionar el botón pedir  números  hasta que el usuario quiera,
 sumar los que son positivos y multiplicar los negativos.*/
 function mostrar()
 {
-	var contador;
-	var respuesta;
-	var sumaPositivos;
-	var multiplicacionNegativos;
-	contador=0;
-	sumaPositivos=0;
-	multiplicacionNegativos=1;
-	respuesta='si';
+	let numero;
+	let contadorPositivos = 0;
+	let multiplicadorNegativos = 1;
+	let respuesta = "si";
 
+	while(respuesta == "si"){
+		numero = parseFloat(prompt("ingrese un numero"));
+		if(numero >= 0){
+			contadorPositivos = contadorPositivos + numero;
+		}
+		else{
+			multiplicadorNegativos = multiplicadorNegativos * numero;
+		}
+		respuesta = prompt("¿quiere seguir? si/no");
+	}
 
-	txtIdSuma.value=sumaPositivos;
-	txtIdProducto.value=multiplicacionNegativos;
-
+	document.getElementById("txtIdSuma").value = contadorPositivos;
+	document.getElementById("txtIdProducto").value = multiplicadorNegativos;
 }//FIN DE LA FUNCIÓN
