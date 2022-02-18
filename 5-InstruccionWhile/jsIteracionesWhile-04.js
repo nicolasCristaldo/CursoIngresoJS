a37aca5542d05ab00e00d65123dd09b30e9b651a
nicolas cristaldo <crist4545@gmail.com>
2022-02-17 22:35:17 -0300

    while 4

--- 5-InstruccionWhile/jsIteracionesWhile-04.js
+++ 5-InstruccionWhile/jsIteracionesWhile-04.js
@@ -1,9 +1,15 @@
-/*
+/* nicolas cristaldo - while 4
 al presionar el botón 
 pedir un número entre 0 y 9 inclusive.*/
 function mostrar()
 {
-	var numeroIngresado;
-	numeroIngresado = prompt("ingrese un número entre 0 y 10.");
+	let numero;
+
+	numero = parseFloat(prompt("ingrese un numero entre 0 y 9"));
+
+	while(numero < 0 || numero > 9){
+		numero = parseFloat(prompt("numero incorrecto, ingrese un numero entre 0 y 9"));
+	}
 	
+	document.getElementById("txtIdNumero").value = numero;
 }//FIN DE LA FUNCIÓN
