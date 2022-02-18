c691f91858b4eaa49e96a4c81e7d8d51eed55e1a
nicolas cristaldo <crist4545@gmail.com>
2022-02-17 22:51:03 -0300

    while 7

--- 5-InstruccionWhile/jsIteracionesWhile-07.js
+++ 5-InstruccionWhile/jsIteracionesWhile-07.js
@@ -1,18 +1,24 @@
-/*
+/* nicolas cristaldo - while 7
 Al presionar el botón pedir  números  hasta que el USUARIO QUIERA 
 e informar la suma acumulada y el promedio.
 */
 function mostrar()
 {
-	var contador;
-	var acumulador;
-	var respuesta;
-	contador=0;
-	acumulador=0;
-	respuesta='si';
+	let promedio;
+	let numero;
+	let respuesta = "si";
+	let acumulador = 0;
+	let i = 0;
 
+	while(respuesta == "si"){
+		numero = parseInt(prompt("ingrese un numero"));
+		acumulador = acumulador + numero;
+		i++;
+		respuesta = prompt("¿quiere seguir? si/no");
+	}
 
-	txtIdSuma.value=acumulador;
-	txtIdPromedio.value=acumulador/contador;
+	promedio = acumulador / i;
 
+	document.getElementById("txtIdSuma").value = acumulador;
+	document.getElementById("txtIdPromedio").value = promedio;
 }//FIN DE LA FUNCIÓN
