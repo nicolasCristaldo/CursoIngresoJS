9794f936c86f67943668b2c8cb9f95f78754a6f1
nicolas cristaldo <crist4545@gmail.com>
2022-02-19 18:00:13 -0300

    while 2

--- 5-InstruccionWhile/jsIteracionesWhile-02.js
+++ 5-InstruccionWhile/jsIteracionesWhile-02.js
@@ -1,14 +1,36 @@
 //nicolas cristaldo - while 2
 /*
-al presionar el botón mostrar 10 repeticiones 
-con números DESCENDENTES, desde el 10 al 1.*/
+Pedir nombre y temperatura hasta que el usuario quiera, mostrar mensaje: 
+" Usted se llama XXX y tiene la temperatura (NORMAL, BAJA, ALTA DEBE AISLARSE)"
+(VALIDAR que sean numeros y los rangos de las temperaturas)*/
 function mostrar()
 {
-	let i = 11;
+	let respuesta;
+	let temperatura;
+	let nombre;
+	let mensaje;
 
-	while(i > 1){
-		i = i - 1;
-		alert(i);
-	}
+	do{
+		nombre = prompt("ingrese su nombre");
+		temperatura = parseFloat(prompt("ingrese su temperatura en grados celcius"));
+		while(isNaN(temperatura) == true || temperatura > 44 || temperatura < 30){
+			temperatura = parseFloat(prompt("es imposible que usted tenga esa temperatura, por favor vuelva a ingresarla"));
+		}
+
+		if(temperatura >= 38){
+			temperatura = "ALTA, DEBE AISLARSE";
+		}
+		else if(temperatura > 35){
+			temperatura = "NORMAL";
+		}
+		else{
+			temperatura = "BAJA"
+		}
+
+		alert("hola " + nombre + ", usted tiene temperatura " + temperatura);
+		
+		respuesta = prompt("desea continuar? s/n");
+
+	}while(respuesta == "s");
 
 }//FIN DE LA FUNCIÓN
