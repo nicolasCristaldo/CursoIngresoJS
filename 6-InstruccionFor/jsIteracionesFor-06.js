81aca540467cb39244f9038e0ac93ae77f9b5132
nicolas cristaldo <crist4545@gmail.com>
2022-02-23 17:37:04 -0300

    for 6

--- 6-InstruccionFor/jsIteracionesFor-06.js
+++ 6-InstruccionFor/jsIteracionesFor-06.js
@@ -1,7 +1,29 @@
 function mostrar()
 {
-	alert("ok");
+	let i;
+	let numero;
+	let pares = 0;
 
+	numero = parseInt(prompt("ingrese un numero"));
+	if(isNaN(numero) == true){
+		numero = parseInt(prompt("error, no ingreso un numero"));
+	}
 
-
+	if(numero > 0){
+		for(i = 1; i <= numero; i++){
+			if((i % 2) == 0){
+				pares++;
+				alert(i);
+			}
+		}
+	}
+	else{
+		for(i = 1; i >= numero; i--){
+			if((i % 2) == 0){
+				pares++;
+				alert(i);
+			}
+		}
+	}
+	alert("cantidaad de pares: " + pares);
 }//FIN DE LA FUNCIÓN
