d641333f6c2a4a4f3b629ffa84fc6bcb9815a5da
nicolas cristaldo <crist4545@gmail.com>
2022-02-23 16:50:06 -0300

    for 4

--- 6-InstruccionFor/jsIteracionesFor-04.js
+++ 6-InstruccionFor/jsIteracionesFor-04.js
@@ -1,7 +1,18 @@
+// nicolas cristaldo - for 4
 function mostrar()
 {
-	alert("ok");
+	let repeticiones;
+	let i;
 
+	repeticiones = parseInt(prompt("ingrese un numero"));
+	while(isNaN(repeticiones) == true){
+		repeticiones = parseInt(prompt("error, no ha ingresado un numero"));
+	}
 
-
+	for(i = 1; i <= repeticiones; i++){
+		alert(i);
+		if(i == 3){
+			break;
+		}
+	}
 }//FIN DE LA FUNCIÓN
