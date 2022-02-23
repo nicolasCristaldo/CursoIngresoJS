48d13fa54ed1867f88d9061c3638bdde116a6bfc
nicolas cristaldo <crist4545@gmail.com>
2022-02-23 17:05:02 -0300

    for 5

--- 6-InstruccionFor/jsIteracionesFor-05.js
+++ 6-InstruccionFor/jsIteracionesFor-05.js
@@ -1,7 +1,14 @@
+//nicolas cristaldo - for 5
 function mostrar()
 {
-	alert("ok");
+	let i;
+	let numero;
 
+	for(i = 0; i < 1; i--){
+		numero = parseInt(prompt("ingrese un numero"));
 
-
+		if(numero == 9){
+			break;
+		}
+	}
 }//FIN DE LA FUNCIÓN
