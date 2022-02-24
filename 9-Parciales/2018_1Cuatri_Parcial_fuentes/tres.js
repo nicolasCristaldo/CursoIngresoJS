3d6d9ed67b172d48f81affa4b636320312349f23
nicolas cristaldo <crist4545@gmail.com>
2022-02-24 11:01:03 -0300

    parcial 2018 3

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,17 @@
+/* nicolas cristaldo - parcial 2018 3
+Pedir por prompt el precio y el porcentaje de descuento, 
+mostrar el precio final con descuento por id.*/
 function mostrar()
 {
+	let precio;
+	let porcentajeDescuento;
+	let descuento;
+	let precioFinal;
 
+	precio = parseFloat(prompt("ingrese el precio"));
+	porcentajeDescuento = parseFloat(prompt("ingrese en porcentaje de descuento"));
+
+	precioFinal = precio - precio * (porcentajeDescuento / 100);
+
+	document.getElementById("elPrecioFinal").value = precioFinal;
 }
