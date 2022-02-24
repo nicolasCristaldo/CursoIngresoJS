ef94e5c00133bac0d7ee1c637e3d83369319abdc
nicolas cristaldo <crist4545@gmail.com>
2022-02-24 10:43:41 -0300

    parcial 2018 1

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,14 @@
-
+//nicolas cristaldo - parcial 2018 1
 function mostrar()
 {
+	let ancho;
+	let largo;
+	let perimetro;
 
+	largo = parseFloat(prompt("ingrese el largo del rectangulo"));
+	ancho = parseFloat(prompt("ingrese el ancho del rectangulo"));
+
+	perimetro = ancho * 2 + largo * 2;
+
+	alert("el perimetro del rectangulo es " + perimetro);
 }
