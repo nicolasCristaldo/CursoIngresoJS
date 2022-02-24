cd2be024c34360209db284b22de186a785fd9b93
nicolas cristaldo <crist4545@gmail.com>
2022-02-24 14:45:19 -0300

    for 7

--- 6-InstruccionFor/jsIteracionesFor-07.js
+++ 6-InstruccionFor/jsIteracionesFor-07.js
@@ -1,7 +1,24 @@
+/*nicolas cristaldo - for 7
+al presionar el botón pedir un número. 
+mostrar los numeros divisores desde el 1 al número ingresado,
+ y mostrar la cantidad de numeros divisores encontrados.*/
 function mostrar()
 {
+	let i;
+	let numero;
+	let divisores = 0;
 
-	alert("ok");
+	numero = parseInt(prompt("ingrese un numero"));
+	while(isNaN(numero) == true){
+		numero = parseInt(prompt("error, no ingreso un numero"));
+	}
 
+		for(i = 1; i <= numero; i++){
+			if((numero % i) == 0){
+				divisores++;
+				console.log(i);
+			}
+		}
+	console.log("cantidad de divisores: " + divisores);
 
 }//FIN DE LA FUNCIÓN
