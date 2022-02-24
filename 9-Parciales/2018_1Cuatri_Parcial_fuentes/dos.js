0f7b93be2aec211d1a9fd06adead34eb8f6a109c
nicolas cristaldo <crist4545@gmail.com>
2022-02-24 10:50:06 -0300

    parcial 2018 2

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,4 +1,16 @@
+/* nicolas cristaldo - parcial 2018 2
+mostrar por alert el mensaje "usted es xxxxx y vive en la localidad de xxxxxxxx".*/
 function mostrar()
 {
-  
+  let nombre;
+  let localidad;
+  let mensaje;
+
+  nombre = document.getElementById("elNombre").value
+  localidad = document.getElementById("laLocalidad").value
+
+  mensaje = "usted es " + nombre + " y vive en la localidad de " + localidad;
+
+  alert(mensaje);
 }
+
