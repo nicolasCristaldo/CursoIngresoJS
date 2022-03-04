6c498c8faccf7ea9518c7957aa7babcc66358e8f
nicolas cristaldo <crist4545@gmail.com>
2022-03-04 11:05:58 -0300

    parcial 2019 1

--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,12 @@
 
 function mostrar()
 {
+	let lados;
+	let perimetro;
 
+	lados = parseFloat(prompt("ingrese cuanto miden los lados"));
+
+	perimetro = lados * 3;
+
+	alert("perimetro: " + perimetro);
 }
