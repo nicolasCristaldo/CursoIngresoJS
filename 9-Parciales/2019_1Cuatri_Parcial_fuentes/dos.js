75a933c7f96b49ae4a17e9a6fdb235dcb1ae9af5
nicolas cristaldo <crist4545@gmail.com>
2022-03-04 11:16:03 -0300

    parcial 2019 2

--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/dos.js
@@ -1,4 +1,23 @@
 function mostrar()
 {
-  
+  let nombreMujer;
+  let nombreHombre;
+  let pesoMujer;
+  let pesoHombre;
+  let pesoTotal;
+  let promedio;
+  let mensaje;
+
+  nombreHombre = prompt("ingrese el nombre del hombre");
+  nombreMujer = prompt("ingrese el nombre de la mujer");
+
+  pesoHombre = parseFloat(prompt("ingrese el peso del hombre"));
+  pesoMujer = parseFloat(prompt("ingrese el peso de la mujer"));
+
+  pesoTotal = pesoMujer + pesoHombre;
+
+  promedio = pesoTotal / 2;
+
+  mensaje = "ustedes se llaman " + nombreMujer + " y " + nombreHombre + " pesan " + pesoMujer + " y " + pesoHombre + " kilos, que sumados son " + pesoTotal + "  kilos y el promedio de peso " + promedio;
+  alert(mensaje);
 }
